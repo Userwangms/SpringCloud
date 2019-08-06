a3c9a98885206043b620aafb7583d37f5e13bdef
wangmaosong <390871264@qq.com>
2019-08-06 13:43:01 +0800

    svn版

---
+++ springcloud_config_svn/src/main/java/com/wxf/springcloud_config_svn/SpringcloudConfigSvnApplication.java
@@ -0,0 +1,13 @@
+package com.wxf.springcloud_config_svn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudConfigSvnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudConfigSvnApplication.class, args);
+    }
+
+}
---
+++ springcloud_config_svn/src/test/java/com/wxf/springcloud_config_svn/SpringcloudConfigSvnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wxf.springcloud_config_svn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcloudConfigSvnApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
